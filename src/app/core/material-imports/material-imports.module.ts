0113ae1720f8375b5a24590d5773ca8ac66f425f
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-10-17 09:31:30 -0300

    feal: new material imports

--- src/app/core/material-imports/material-imports.module.ts
+++ src/app/core/material-imports/material-imports.module.ts
@@ -1,26 +1,57 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from "@angular/material/button";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
+  declarations: [],
   imports: [
     CommonModule,
+    MatToolbarModule,
+    MatIconModule,
     MatButtonModule,
-    MatFormFieldModule,
-    MatSelectModule,
+    MatSidenavModule,
+    MatListModule,
+    MatDialogModule,
     MatInputModule,
-    MatGridListModule
+    MatSelectModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   exports: [
+    MatToolbarModule,
+    MatIconModule,
     MatButtonModule,
-    MatFormFieldModule,
-    MatSelectModule,
+    MatSidenavModule,
+    MatListModule,
+    MatDialogModule,
     MatInputModule,
-    MatGridListModule
+    MatSelectModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class MaterialImportsModule { }
